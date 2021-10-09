400245c824a29e807e8c2890c0ad0615f0310837
Kerem Berk Bayraktar <keremberk53@gmail.com>
2021-10-09 20:24:05 +0300

    .Net Mvc Güncel Döviz Kurları

---
+++ DovizKuru/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using DovizKuru.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DovizKuru.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            DovizKuruIslemleri islemler = new DovizKuruIslemleri();
+            IEnumerable<DovizKuruModel> liste = islemler.DovizKurlari();
+            return View(liste);
+        }
+    }
+}
---
+++ DovizKuru/Models/DovizKuruModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DovizKuru.Models
+{
+    public class DovizKuruModel
+    {
+        public string CurrencyCode { get; set; }
+        public string Unit { get; set; }
+        public string Isim { get; set; }
+        public string ForexBuying { get; set; }
+        public string ForexSelling { get; set; }
+    }
+}
